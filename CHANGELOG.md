efd3b8877ee37cf645a5dd6dc48a22f20eb356ab
Alex Koutmos <akoutmos@gmail.com>
2018-11-30 22:20:01 -0500

    Version bump and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.0] - 2018-11-30
+
+### Fixed
+
+- Umbrella project exit status code
+
 ## [0.1.0] - 2018-10-04
 
 ### Added
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Doctor.MixProject do
   def project do
     [
       app: :doctor,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       name: "Doctor",
       source_url: "https://github.com/akoutmos/doctor",
